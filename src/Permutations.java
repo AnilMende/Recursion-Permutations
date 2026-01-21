692a27691080213f0b8913c2572537d743aaa357
Anil <anilmende2002@email.com>
2026-01-21 23:39:10 +0530

    output as an ArrayList

--- src/Permutations.java
+++ src/Permutations.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Permutations {
     public static void main(String[] args) {
-        permutations("","abc");
+        //permutations("","abc");
+        ArrayList<String> res = permutationsList("", "abc");
+        System.out.println(res);
     }
 
     static void permutations(String p, String up){
@@ -18,5 +22,26 @@ public class Permutations {
             //skip the character in the unprocessed string
             permutations(first + ch + second, up.substring(1));
         }
+    }
+
+    //Returning the Output as an ArrayList
+    static ArrayList<String> permutationsList(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> res = new ArrayList<>();
+
+        char ch = up.charAt(0);
+
+        for(int i=0;i <= p.length();i++){
+            String first = p.substring(0,i);
+            String second = p.substring(i,p.length());
+            res.addAll(permutationsList(first + ch + second, up.substring(1)));
+        }
+
+        return res;
     }
 }
