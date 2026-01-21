f2b8ee21d73528827c52cc756720cd9506a3f900
Anil <anilmende2002@email.com>
2026-01-21 23:54:53 +0530

    permutations listing counting

--- src/Permutations.java
+++ src/Permutations.java
@@ -5,8 +5,11 @@ public class Permutations {
         //permutations("","abc");
         ArrayList<String> res = permutationsList("", "abc");
         System.out.println(res);
+
+        System.out.println(permutationsCount("", "abcd"));
     }
 
+    //listing all the valid permutations
     static void permutations(String p, String up){
         if(up.isEmpty()){
             System.out.println(p);
@@ -43,5 +46,22 @@ public class Permutations {
         }
 
         return res;
+    }
+
+    //Permutations Count
+    static int permutationsCount(String p, String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+
+        int count = 0;
+        char ch = up.charAt(0);
+        for(int i=0;i <= p.length();i++){
+            String first = p.substring(0,i);
+            String second = p.substring(i, p.length());
+
+            count = count + permutationsCount(first + ch + second, up.substring(1));
+        }
+        return count;
     }
 }
